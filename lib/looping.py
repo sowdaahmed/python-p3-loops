3615f47f9fb823f7049f430b39c43dcaddf96228
Sowda Ahmed <ayanmoha4949@gmail.com>
2025-06-11 05:01:06 -0700

    done

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,27 @@
+
 #!/usr/bin/env python3
 
+import time
+
 def happy_new_year():
-    # code goes here!
-    pass
+    for i in range(10, 0, -1):
+        print(i)
+        # Optional: add a delay for countdown effect
+        # time.sleep(1)
+    print("Happy New Year!")
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [num ** 2 for num in int_list]
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
